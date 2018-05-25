3a7040d41b23cd33a577832b3e2dadbce51082e5
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-05-25 16:33:07 +0300

    убрала самовызывающуюся функцию

---
+++ src/other/01-fizzbuzz.js
@@ -0,0 +1,36 @@
+console.log('=== fizzBuzzV1 ===');
+const fzbz = (i) => {
+  if (i % 15 === 0) {
+    return 'FizzBuzz';
+  } else if (i % 3 === 0) {
+    return 'Fizz';
+  } else if (i % 5 === 0) {
+    return 'Buzz';
+  }
+  return i;
+};
+const fizzBuzz = () => {
+  for (let i = 0; i <= 20; i++) {
+    console.log(fzbz(i));
+  }
+};
+fizzBuzz();
+
+console.log('=== fizzBuzzV2 ===');
+const fizzBuzzV3 = (num) => {
+  for (let i = 0; i <= num; i++) {
+    let str = '';
+    if (i % 3 === 0) {
+      str = 'Fizz';
+    }
+    if (i % 5 === 0) {
+      str += 'Buzz';
+    }
+    if (str === '') {
+      str = i.toString();
+    }
+    console.log(str);
+  }
+};
+fizzBuzzV3(20);
+
