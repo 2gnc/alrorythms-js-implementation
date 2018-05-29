33b4f9e84f191f31469e72c7defaf48bba8264c1
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-05-29 14:39:44 +0300

    удалено лишнее из гита

--- src/other/02-harmless-ransom-note.test.js
+++ src/other/02-harmless-ransom-note.test.js
@@ -6,42 +6,47 @@ const text = 'мама не ела раму и вообще не любит мы
 const text2 = 'мама не ела раму и вообще не любит c мыло масло';
 const text3 = 'мама не ела раму и вообще не любит с мылом маслом с';
 
-test('проверка на неполное совпадение v1', () => {
-  expect(hn1.hn1(note, text)).toBe(false);
+describe('Версия 1', () => {
+  it('неполное совпадение', () => {
+    expect(hn1.hn1(note, text)).toBe(false);
+  });
+
+  it('полное совпадение но нехватку одинаковых слов', () => {
+    expect(hn1.hn1(note, text2)).toBe(false);
+  });
+
+  it('полное совпадение', () => {
+    expect(hn1.hn1(note, text3)).toBe(true);
+  });
+
+  it('пустой текст в записке', () => {
+    expect(hn1.hn1('', text)).toBe(false);
+  });
+
+  it('пустой текст в заметке', () => {
+    expect(hn1.hn1(note, '')).toBe(false);
+  });
 });
 
-test('проверка на полное совпадение но нехватку одинаковых слов v1', () => {
-  expect(hn1.hn1(note, text2)).toBe(false);
+describe('Версия 2', () => {
+  test('неполное совпадение', () => {
+    expect(hn1.hn2(note, text)).toBe(false);
+  });
+
+  test('полное совпадение но нехватка одинаковых слов', () => {
+    expect(hn1.hn2(note, text2)).toBe(false);
+  });
+
+  test('полное совпадение', () => {
+    expect(hn1.hn2(note, text3)).toBe(true);
+  });
+
+  test('пустой текст в записке', () => {
+    expect(hn1.hn2('', text)).toBe(false);
+  });
+
+  it('пустой текст в заметке', () => {
+    expect(hn1.hn2(note, '')).toBe(false);
+  });
 });
 
-test('проверка на полное совпадение v1', () => {
-  expect(hn1.hn1(note, text3)).toBe(true);
-});
-
-test('проверка на пустой текст в записке v1', () => {
-  expect(hn1.hn1('', text)).toBe(false);
-});
-
-test('проверка на пустой текст в заметке v1', () => {
-  expect(hn1.hn1(note, '')).toBe(false);
-});
-
-test('проверка на неполное совпадение v2', () => {
-  expect(hn1.hn2(note, text)).toBe(false);
-});
-
-test('проверка на полное совпадение но нехватку одинаковых слов v2', () => {
-  expect(hn1.hn2(note, text2)).toBe(false);
-});
-
-test('проверка на полное совпадение v2', () => {
-  expect(hn1.hn2(note, text3)).toBe(true);
-});
-
-test('проверка на пустой текст в записке v2', () => {
-  expect(hn1.hn2('', text)).toBe(false);
-});
-
-test('проверка на пустой текст в заметке v2', () => {
-  expect(hn1.hn2(note, '')).toBe(false);
-});
--- src/other/03-is-palindrome.test.js
+++ src/other/03-is-palindrome.test.js
@@ -1,81 +1,86 @@
 const p = require('./03-is-palindrome');
 
-test('Версия 1: - level', () => {
-  expect(p.v1('level')).toBe(true);
+describe('Версия 2', () => {
+  it('level', () => {
+    expect(p.v2('level')).toBe(true);
+  });
+
+  it('Level', () => {
+    expect(p.v2('Level')).toBe(true);
+  });
+
+  it('leve', () => {
+    expect(p.v2('leve')).toBe(false);
+  });
+
+  it('leel', () => {
+    expect(p.v2('leel')).toBe(true);
+  });
+
+  it('le3el', () => {
+    expect(p.v2('le3el')).toBe(true);
+  });
+
+  it('le33el', () => {
+    expect(p.v2('le33el')).toBe(true);
+  });
+
+  it('1', () => {
+    expect(p.v2('1')).toBe(true);
+  });
+
+  it('А Лида гадила', () => {
+    expect(p.v2('А Лида гадила')).toBe(true);
+  });
+
+  it('race car', () => {
+    expect(p.v2('race car')).toBe(true);
+  });
+
+  it('Madam, I`m Adam', () => {
+    expect(p.v2('Madam, I`m Adam')).toBe(true);
+  });
 });
 
-test('Версия 1: - Level', () => {
-  expect(p.v1('Level')).toBe(true);
-});
 
-test('Версия 1: - leve', () => {
-  expect(p.v1('leve')).toBe(false);
-});
+describe('Версия 1', () => {
+  it('level', () => {
+    expect(p.v1('level')).toBe(true);
+  });
 
-test('Версия 1: - leel', () => {
-  expect(p.v1('leel')).toBe(true);
-});
+  it('Level', () => {
+    expect(p.v1('Level')).toBe(true);
+  });
 
-test('Версия 1: - le3el', () => {
-  expect(p.v1('le3el')).toBe(true);
-});
+  it('leve', () => {
+    expect(p.v1('leve')).toBe(false);
+  });
 
-test('Версия 1: - le33el', () => {
-  expect(p.v1('le33el')).toBe(true);
-});
+  it('leel', () => {
+    expect(p.v1('leel')).toBe(true);
+  });
 
-test('Версия 1: - 1', () => {
-  expect(p.v1('1')).toBe(true);
-});
+  it('le3el', () => {
+    expect(p.v1('le3el')).toBe(true);
+  });
 
-test('Версия 1: - А Лида гадила', () => {
-  expect(p.v1('А Лида гадила')).toBe(true);
-});
+  it('le33el', () => {
+    expect(p.v1('le33el')).toBe(true);
+  });
 
-test('Версия 1: - race car', () => {
-  expect(p.v1('race car')).toBe(true);
-});
+  it('1', () => {
+    expect(p.v1('1')).toBe(true);
+  });
 
-test('Версия 1: - Madam, I`m Adam', () => {
-  expect(p.v1('Madam, I`m Adam')).toBe(true);
-});
+  it('А Лида гадила', () => {
+    expect(p.v1('А Лида гадила')).toBe(true);
+  });
 
-test('Версия 2: - level', () => {
-  expect(p.v2('level')).toBe(true);
-});
+  it('race car', () => {
+    expect(p.v1('race car')).toBe(true);
+  });
 
-test('Версия 2: - Level', () => {
-  expect(p.v2('Level')).toBe(true);
-});
-
-test('Версия 2: - leve', () => {
-  expect(p.v2('leve')).toBe(false);
-});
-
-test('Версия 2: - leel', () => {
-  expect(p.v2('leel')).toBe(true);
-});
-
-test('Версия 2: - le3el', () => {
-  expect(p.v2('le3el')).toBe(true);
-});
-
-test('Версия 2: - le33el', () => {
-  expect(p.v2('le33el')).toBe(true);
-});
-
-test('Версия 2: - 1', () => {
-  expect(p.v2('1')).toBe(true);
-});
-
-test('Версия 2: - А Лида гадила', () => {
-  expect(p.v2('А Лида гадила')).toBe(true);
-});
-
-test('Версия 2: - race car', () => {
-  expect(p.v2('race car')).toBe(true);
-});
-
-test('Версия 2: - Madam, I`m Adam', () => {
-  expect(p.v2('Madam, I`m Adam')).toBe(true);
+  it('Madam, I`m Adam', () => {
+    expect(p.v1('Madam, I`m Adam')).toBe(true);
+  });
 });
