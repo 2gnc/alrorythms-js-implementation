8a321af11da67432ffaa16a080378030c823d119
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-05-29 14:23:16 +0300

    добавила новую версию алгоритма

--- src/other/03-is-palindrome.js
+++ src/other/03-is-palindrome.js
@@ -6,10 +6,19 @@ const isPalindromeV1 = (string) => {
   strArr.forEach((char) => {
     if (validChar.includes(char)) lettersArr.push(char);
   });
+  return lettersArr.join('') === lettersArr.reverse().join('');
+};
+
+const isPalindromeV2 = (string) => {
+  const strArr = string.toLowerCase().split('');
+  const validChar = 'qwertyuiopasdfghjklzxcvbnmйцукенгшщзхъфывапролджэячсмитьбюё'.split('');
+
+  const lettersArr = strArr.filter(char => validChar.includes(char));
 
   return lettersArr.join('') === lettersArr.reverse().join('');
 };
 
-isPalindromeV1('Madam, I`m Adam');
-
 module.exports.v1 = isPalindromeV1;
+module.exports.v2 = isPalindromeV2;
+
+// сложность O(n)
--- src/other/03-is-palindrome.test.js
+++ src/other/03-is-palindrome.test.js
@@ -39,3 +39,43 @@ test('Версия 1: - race car', () => {
 test('Версия 1: - Madam, I`m Adam', () => {
   expect(p.v1('Madam, I`m Adam')).toBe(true);
 });
+
+test('Версия 2: - level', () => {
+  expect(p.v2('level')).toBe(true);
+});
+
+test('Версия 2: - Level', () => {
+  expect(p.v2('Level')).toBe(true);
+});
+
+test('Версия 2: - leve', () => {
+  expect(p.v2('leve')).toBe(false);
+});
+
+test('Версия 2: - leel', () => {
+  expect(p.v2('leel')).toBe(true);
+});
+
+test('Версия 2: - le3el', () => {
+  expect(p.v2('le3el')).toBe(true);
+});
+
+test('Версия 2: - le33el', () => {
+  expect(p.v2('le33el')).toBe(true);
+});
+
+test('Версия 2: - 1', () => {
+  expect(p.v2('1')).toBe(true);
+});
+
+test('Версия 2: - А Лида гадила', () => {
+  expect(p.v2('А Лида гадила')).toBe(true);
+});
+
+test('Версия 2: - race car', () => {
+  expect(p.v2('race car')).toBe(true);
+});
+
+test('Версия 2: - Madam, I`m Adam', () => {
+  expect(p.v2('Madam, I`m Adam')).toBe(true);
+});
