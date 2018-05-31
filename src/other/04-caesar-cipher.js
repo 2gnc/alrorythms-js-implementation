84175e5cc1837b226db67898cd9302034d30e6c3
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-05-31 16:04:00 +0300

    добавила алгоритм и тесты

--- src/other/04-caesar-cipher.js
+++ src/other/04-caesar-cipher.js
@@ -1,7 +1,23 @@
 const caesarCipher = (str, num) => {
-  return null;
+  const lcStr = str.toLowerCase();
+  let newStr = '';
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const number = num % 26;
+  for (let i = 0; i < lcStr.length; i++) {
+    if (lcStr[i] === ' ') {
+      newStr += ' ';
+    } else {
+      const currIdx = alphabet.indexOf(lcStr[i]);
+      let newIdx = currIdx + number;
+      if (newIdx > 25) newIdx -= 26;
+      if (newIdx < 0) newIdx += 26;
+      if (str[i] === str[i].toUpperCase()) {
+        newStr += alphabet[newIdx].toUpperCase();
+      } else {
+        newStr += alphabet[newIdx];
+      }
+    }
+  }
+  return newStr;
 };
-
-caesarCipher('zoo keeper', 5);
-
 module.exports = caesarCipher;
--- src/other/04-caesar-cipher.test.js
+++ src/other/04-caesar-cipher.test.js
@@ -2,20 +2,28 @@ const caesar = require('./04-caesar-cipher');
 
 describe('шифр Цезаря', () => {
   it('сдвиг на положительное число', () => {
-
+    expect(caesar('zoo keeper', 5)).toEqual('ett pjjujw');
+  });
+  it('есть большие буквы', () => {
+    expect(caesar('Zoo keeper', 5)).toEqual('Ett pjjujw');
   });
   it('сдвиг на отрицательное число', () => {
-
+    expect(caesar('zoo keeper', -1)).toEqual('ynn jddodq');
   });
-
   it('сдвиг на положительное число, преход через конец алфавита', () => {
-
+    expect(caesar('xwing', 5)).toEqual('cbnsl');
   });
   it('сдвиг на отрицательное число, переход через начало алфавита', () => {
-
+    expect(caesar('abba', -2)).toEqual('yzzy');
   });
-
   it('сдвиг на 0', () => {
-
+    expect(caesar('abba', 0)).toEqual('abba');
+  });
+  it('сдвиг на 300', () => {
+    expect(caesar('abba', 300)).toEqual('oppo');
+  });
+  it('длинны строк одинаковые', () => {
+    const a = caesar('zoo kepper', 5);
+    expect(a.length === 10).toBe(true);
   });
 });
