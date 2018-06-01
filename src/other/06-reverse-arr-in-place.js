6f4c657696637713df5b58da4297ecdfdaa6f3fe
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-06-01 15:59:45 +0300

    небольшие улучшения

--- src/other/06-reverse-arr-in-place.js
+++ src/other/06-reverse-arr-in-place.js
@@ -1,6 +1,7 @@
 const reverseArrInPlace = arr => {
   const al = arr.length - 1;
-  for (let i = 0; i < arr.length / 2; i++) {
+  const mi = Math.floor(arr.length / 2);
+  for (let i = 0; i < mi; i++) {
     [arr[i], arr[al - i]] = [arr[al - i], arr[i]];
   }
   return arr;
