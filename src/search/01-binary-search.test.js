56fb1ee364ec013a933ac716f19525706986ed02
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-06-02 22:44:31 +0300

    добавила тесты

--- src/search/01-binary-search.test.js
+++ src/search/01-binary-search.test.js
@@ -4,10 +4,16 @@ describe('бинарный поиск', () => {
   it('элемент есть в массиве', () => {
     expect(bin([1, 2, 3], 3)).toBeTruthy();
   });
+  it('элемент есть в массиве', () => {
+    expect(bin([1, 2, 3], 1)).toBeTruthy();
+  });
   it('элемента нет в массиве', () => {
     expect(bin([1, 2, 3], 4)).not.toBeTruthy();
   });
   it('пустой массив', () => {
     expect(bin([], 3)).not.toBeTruthy();
+  });
+  it('массив из одного элемента', () => {
+    expect(bin([3], 3)).not.toBeTruthy();
   });
 });
