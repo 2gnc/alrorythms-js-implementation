e2cb022c0e76dd3c635dfded0e339e368b31918a
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-06-02 23:43:12 +0300

    мелкие правки

--- src/other/05-reverse-words.js
+++ src/other/05-reverse-words.js
@@ -1,6 +1,6 @@
-const reverseWords = (str) => {
+const reverseWords = str => {
   const wordsArr = str.split(' ');
-  const reversedArr = wordsArr.map((word) => {
+  const reversedArr = wordsArr.map(word => {
     let newWord = '';
     for (let i = word.length - 1; i >= 0; i--) {
       newWord += word[i];
