ab76b2a230a15d63c593fe0195c1ae643f93593c
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-06-04 22:06:22 +0300

    мелкие правки

--- src/sortings/04-insert-sort.js
+++ src/sortings/04-insert-sort.js
@@ -1,6 +1,6 @@
 const sort = arr => {
   const sorted = [...arr];
-  for (let i = 1; i <=sorted.length; i++) {
+  for (let i = 1; i < sorted.length; i++) {
     for (let k = i; k >= 0; k--) {
       if (sorted[k] < sorted[k - 1]) [sorted[k], sorted[k - 1]] = [sorted[k - 1], sorted[k]];
     }
