fd8081ecc5bdc1335cbe62bc075582a7cb4e39b8
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-06-05 18:55:17 +0300

    добавила связанный список и его методы

---
+++ src/structures/01-linked-list.js
@@ -0,0 +1,129 @@
+/**
+ * @description создает связанный список
+ */
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
+
+/**
+ * @description создает узел связанного списка
+ * @param {*} value содержимое узла
+ * @param {Object.Node} next следующий узел
+ * @param {Object.Node} prev предыдущий узел
+ */
+function Node(value, next, prev) {
+  this.value = value;
+  this.next = next;
+  this.prev = prev;
+}
+
+/**
+ * @description добавляет новый узел в начало списка
+ * @param {*} value содержимое узла
+ */
+LinkedList.prototype.addToHead = function (value) {
+  const newNode = new Node(value, this.head, null);
+  if (this.head) {
+    this.head.prev = newNode;
+  } else {
+    this.tail = newNode;
+  }
+  this.head = newNode;
+};
+
+/**
+ * @description добавляет новый узел в конец списка
+ * @param {*} value содержимое узла
+ */
+LinkedList.prototype.addToTail = function (value) {
+  const newNode = new Node(value, null, this.tail);
+  if (this.tail) {
+    this.tail.next = newNode;
+  } else {
+    this.head = newNode;
+  }
+  this.tail = newNode;
+};
+
+/**
+ * @description удаляет узел из начала списка
+ * @returns значение удаленного узла
+ */
+LinkedList.prototype.removeHead = function () {
+  if (!this.head) return null;
+  const oldVal = this.head.value;
+  this.head = this.head.next;
+  if (this.head) {
+    this.head.prev = null;
+  } else {
+    this.tail = null;
+  }
+  return oldVal;
+};
+
+/**
+ * @description удаляет узел с конца списка
+ * @returns значение удаленного узла
+ */
+LinkedList.prototype.removeTail = function () {
+  if (!this.tail) return null;
+  const oldVal = this.tail.value;
+  this.tail = this.tail.prev;
+  if (this.tail) {
+    this.tail.next = null;
+  } else {
+    this.head = null;
+  }
+  return oldVal;
+};
+
+/**
+ * @description ищет указанное значение в узлах списка с начала
+ * @param {*} searchValue что нужно найти
+ * @returns {searchValue||null}
+ */
+LinkedList.prototype.search = function (searchValue) {
+  let currentNode = this.head;
+  while (currentNode) {
+    if (currentNode.value === searchValue) return currentNode.value;
+    currentNode = currentNode.next;
+  }
+  return null;
+};
+
+/**
+ * @description выводит в консоль все значения узлов начиная с головы
+ */
+LinkedList.prototype.print = function () {
+  let current = this.head;
+  while (current) {
+    console.log(current.value);
+    current = current.next;
+  }
+};
+
+/**
+ * @description разворачивает список
+ */
+LinkedList.prototype.reverse = function () {
+  const tmpVal = this.head.value;
+  this.head.value = this.tail.value;
+  this.tail.value = tmpVal;
+  let currentNode = this.head.next;
+  while (currentNode.next.next) {
+    const tmp = currentNode.value;
+    currentNode.value = currentNode.next.value;
+    currentNode.next.value = tmp;
+    currentNode = currentNode.next;
+  }
+};
+
+const ll = new LinkedList();
+ll.addToHead(100);
+ll.addToHead(200);
+ll.addToHead(300);
+ll.addToHead(400);
+ll.print();
+ll.reverse();
+ll.print();
